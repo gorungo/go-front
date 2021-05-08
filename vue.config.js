de458b9075b2abc116e966aaf21441a82ff7517f
inkas <denis.g.petrov@gmail.com>
2021-05-08 18:38:27 +1000

    update manifest

--- vue.config.js
+++ vue.config.js
@@ -15,8 +15,8 @@ module.exports = {
     // настройки манифеста
     manifestOptions: {
       display: 'landscape',
-      background_color: '#42B883'
-      // ...другие настройки манифеста...
+      background_color: '#42B883',
+      icons: [ { 'src': './images/icons/android-chrome-192x192.png', 'sizes': '192x192', 'type': 'image/png' }, { 'src': './images/icons/android-chrome-512x512.png', 'sizes': '512x512', 'type': 'image/png' }, { 'src': './images/icons/android-chrome-maskable-192x192.png', 'sizes': '192x192', 'type': 'image/png', 'purpose': 'maskable' }, { 'src': './images/icons/android-chrome-maskable-512x512.png', 'sizes': '512x512', 'type': 'image/png', 'purpose': 'maskable' } ]
     },
 
     // настройка workbox-плагина
