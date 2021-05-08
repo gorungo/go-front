c20e9cbf4d0720a335df0f7e64cae62edefb333e
inkas <denis.g.petrov@gmail.com>
2021-05-08 18:55:32 +1000

    update manifest

--- vue.config.js
+++ vue.config.js
@@ -16,10 +16,19 @@ module.exports = {
     manifestOptions: {
       display: 'landscape',
       background_color: '#42B883',
-      icons: [ { 'src': './images/icons/android-chrome-192x192.png', 'sizes': '192x192', 'type': 'image/png' }, { 'src': './images/icons/android-chrome-512x512.png', 'sizes': '512x512', 'type': 'image/png' }, { 'src': './images/icons/android-chrome-maskable-192x192.png', 'sizes': '192x192', 'type': 'image/png', 'purpose': 'maskable' }, { 'src': './images/icons/android-chrome-maskable-512x512.png', 'sizes': '512x512', 'type': 'image/png', 'purpose': 'maskable' } ]
+
+    },
+
+    iconPath: {
+      favicon32: 'images/icons/favicon-32x32.png',
+      favicon16: 'images/icons/favicon-16x16.png',
+      appleTouchIcon: 'images/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'images/icons/safari-pinned-tab.svg',
+      msTileImage: 'images/icons/msapplication-icon-144x144.png'
     },
 
     // настройка workbox-плагина
     workboxPluginMode: 'GenerateSW',
+    manifestPath: '/js/manifest.json'
   }
 }
