2d2224547b84f533115629b72e199a3a0fd74071
inkas <denis.g.petrov@gmail.com>
2021-05-17 16:55:34 +1000

    update profile booking

--- src/app/components/idea/IdeaBookingModal.vue
+++ src/app/components/idea/IdeaBookingModal.vue
@@ -5,7 +5,7 @@
       @closed="handleDialogClosed"
       :fullscreen = "isMobile"
   >
-    <div class="booking-modal" v-if="idea && date && this.idea.relationships.author.attributes.whatsapp">
+    <div class="booking-modal" v-if="idea && date && this.idea.relationships.author.attributes.booking_whatsapp">
       <div class="booking-modal__label">{{$t('booking.bookingPlaceIn')}}</div>
       <div class="booking-modal__title">{{idea.attributes.title}}</div>
       <div class="booking-modal__label">{{$t('booking.bookingDateTime')}}</div>
@@ -17,15 +17,15 @@
         <input class="form-control" id="people-count" type="number" min="1" max="10" step="1" name="people_count" v-model="peopleCount"/>
       </div>
       <p>После нажатия на "Забронировать" вы будете переведены в приложение Whatsapp, где сможете договориться об участии в мероприятии.</p>
-      <span slot="footer" class="dialog-footer">
-      <button type="button" @click="handleCancelClick" class="btn btn-secondary">{{$t('text.close')}}</button>
-      <a target="_blank" :href="whatsAppUrl" rel="noopener" class="btn btn-primary">{{$t('booking.buttonBook')}}</a>
+    </div>
+    <div class="booking-modal" v-if="idea && date && !this.idea.relationships.author.attributes.booking_whatsapp">
+      <p class="booking-modal__title">{{idea.attributes.booking_info ? idea.attributes.booking_info : $t('idea.bookingInfoDefault')}}</p>
+      <p class="booking-modal__title">{{idea.attributes.booking_contacts}}</p>
+    </div>
+    <span slot="footer" class="dialog-footer">
+        <button type="button" @click="handleCancelClick" class="btn btn-secondary">{{$t('text.close')}}</button>
+        <a v-if="idea && date && this.idea.relationships.author.attributes.booking_whatsapp" target="_blank" :href="whatsAppUrl" rel="noopener" class="btn btn-primary">{{$t('booking.buttonBook')}}</a>
     </span>
-    </div>
-    <div v-else>
-      <div class="booking-modal__title">{{idea.attributes.booking_info ? idea.attributes.booking_info : $t('idea.bookingInfoDefault')}}</div>
-      <div class="booking-modal__title">{{idea.attributes.booking_contacts}}</div>
-    </div>
   </app-dialog>
 
 </template>
@@ -57,7 +57,7 @@ export default {
   computed: {
     whatsAppUrl(){
       if(!this.idea || !this.idea.relationships.author || !this.date) return ''
-      let bookingPhone = this.idea.relationships.author.attributes.whatsapp ? this.idea.relationships.author.attributes.whatsapp : '79635150150'
+      let bookingPhone = this.idea.relationships.author.attributes.booking_whatsapp
       let text = `Привет, хочу забронировать место в "${this.idea.attributes.title}". \n Дата и время: ${this.localeDate(this.date)} ${this.$t('text.at')} ${this.date.attributes.start_time}. Количество участников: ${this.peopleCount}. Бронирование с Gorungo.ru`
       return `https://api.whatsapp.com/send?phone=+${bookingPhone}&text=${text}`
     },
