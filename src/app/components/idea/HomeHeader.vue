b37e418cab8bc8a071e66cf02c8ea838c54625d1
inkas <denis.g.petrov@gmail.com>
2021-08-22 12:24:56 +1000

    update z-index

--- src/app/components/idea/HomeHeader.vue
+++ src/app/components/idea/HomeHeader.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="HomeHeader" class="home-header container-100 home-header__vs-background" style="background-image: url('/images/bg/mountains_blue.svg');">
+<div id="HomeHeader" class="home-header container-100">
   <div class="home-header__w">
     <h1 class="home-header__title">{{$t('idea.siteIntroText')}}</h1>
     <main-filter />
