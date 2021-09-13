b7e98b9dcc449257fe925d90ef052b9b917fef23
inkas <denis.g.petrov@gmail.com>
2021-09-13 17:59:34 +1000

    add manual sw

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
 
     // настройка workbox-плагина
     workboxPluginMode: 'InjectManifest',
-    swSrc: 'public/service-worker.js',
+    swSrc: 'src/service-worker.js',
     manifestPath: 'pwa/manifest.json',
 
     iconPaths: {
