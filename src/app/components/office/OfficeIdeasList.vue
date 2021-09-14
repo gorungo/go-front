65d1267df9e3900c428c6fd33f81bdd8f655d7f1
inkas <denis.g.petrov@gmail.com>
2021-09-14 11:27:58 +1000

    add ideas sort in OfficeIdeasList.vue

--- src/app/components/office/OfficeIdeasList.vue
+++ src/app/components/office/OfficeIdeasList.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="office-ideas-list">
-    <div v-if="value.length > 0">
+    <div v-if="activeIdeas.length > 0">
+      <h4>Активные идеи</h4>
       <list-item-line
-          v-for="idea in value"
+          v-for="idea in activeIdeas"
+          :key="idea.hid"
+          :item="idea"
+          @edit="$emit('edit', $event)"
+          @delete="$emit('delete', $event)"
+      />
+    </div>
+    <div v-if="value.length > 0" class="mt-2">
+      <h4>Время идеи прошло</h4>
+      <list-item-line
+          v-for="idea in outDateIdeas"
+          :key="idea.hid"
+          :item="idea"
+          @edit="$emit('edit', $event)"
+          @delete="$emit('delete', $event)"
+      />
+    </div>
+    <div v-if="value.length > 0" class="mt-2">
+      <h4>Черновики</h4>
+      <list-item-line
+          v-for="idea in notPublishedIdeas"
           :key="idea.hid"
           :item="idea"
           @edit="$emit('edit', $event)"
@@ -30,6 +51,33 @@ export default {
       loading: false,
     }
   },
+
+  computed: {
+    activeIdeas(){
+      return this.value.filter( idea => {
+        return this.isPublished(idea) && !this.outDate(idea)
+      })
+    },
+    outDateIdeas(){
+      return this.value.filter( idea => {
+        return this.isPublished(idea) && this.outDate(idea)
+      })
+    },
+    notPublishedIdeas(){
+      return this.value.filter( idea => {
+        return !this.isPublished(idea)
+      })
+    }
+  },
+
+  methods: {
+    isPublished(idea){
+      return idea.attributes.is_published
+    },
+    outDate(idea){
+      return idea.relationships.future_dates.length === 0
+    }
+  }
 
 
 }
