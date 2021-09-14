2d1551575ca9af8cfded5a27de0eea20eeb605dc
inkas <denis.g.petrov@gmail.com>
2021-09-14 16:09:20 +1000

    update sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,10 +11,28 @@
  * See https://goo.gl/2aRDsh
  */
 
-if (workbox) {
-  workbox.core.setCacheNameDetails({prefix: "gorungo"});
-  workbox.precaching.precacheAndRoute(self.__precacheManifest);
-}
+// if (workbox) {
+//   workbox.core.setCacheNameDetails({prefix: "gorungo"})
+//   workbox.precaching.precacheAndRoute(self.__precacheManifest)
+// }
+
+staticCacheName = 'gorungo-static-cache'
+dynamicCacheName = 'gorungo-api-cache'
+
+self.addEventListener('install', async event => {
+  console.log(self.__precacheManifest)
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(self.__precacheManifest.map(i => i.url))
+})
+
+self.addEventListener('activate', async event => {
+  const cachesNames = await caches.keys()
+  await Promise.all(
+    cachesNames
+      .filter(name => name !== staticCacheName)
+      .filter(name => name !== dynamicCacheName)
+  )
+})
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -23,7 +41,39 @@ self.addEventListener('message', (event) => {
 });
 
 self.addEventListener('fetch', event => {
-  //console.log(event.request.url)
+  const {request} = event
+  const url = new URL(request.url)
+  if(url.origin === location.origin){
+    event.respondWith(cacheFirst(request))
+  }else{
+     event.respondWith(networkFirst(request))
+  }
 })
+
+async function cacheFirst(request){
+  const cached = await caches.match(request)
+  return cached ?? await fetch(request)
+}
+
+async function networkFirst(request){
+  const cache = await caches.open(dynamicCacheName)
+
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+    return response
+
+  } catch (e) {
+    const cached = cache.match(request)
+
+    if(!cached){
+      document.dispatchEvent(new CustomEvent('appOffline'))
+      console.log('Offline')
+      return
+    }
+    return cached
+
+  }
+}
 
 
