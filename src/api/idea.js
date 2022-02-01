d98affb325a0ace40ab11e3fd817eeb238ba354f
inkas <denis.g.petrov@gmail.com>
2022-02-02 06:41:31 +1000

    change page limit

--- src/api/idea.js
+++ src/api/idea.js
@@ -16,7 +16,7 @@ export const getIdea = (ideaHid, options = {}) => {
 export const getIdeas = async (options = {}) => {
     const defaultOptions = {
         page: 1,
-        limit: 40,
+        limit: 60,
         sort: 'default',
         sortDirection: 'default',
         include: 'price,author,futureDates',
